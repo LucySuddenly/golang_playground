28c55bb9c7c5aa67ecb4cdeca13766473150621a
Lucy Suddenly <suddenlylucy@gmail.com>
2019-12-26 15:18:59 -0800

    maps!

--- hello.go
+++ hello.go
@@ -5,7 +5,12 @@ import (
 )
 
 func main() {
-	a := []int{5, 4, 3, 2, 1}
-	a = append(a, 0)
-	fmt.Println(a)
+	// a := []int{5, 4, 3, 2, 1}
+	// a = append(a, 0)
+	object := make(map[string]int)
+	object["key1"] = 1
+	object["key2"] = 2
+	object["banana"] = 9999
+
+	fmt.Println(object)
 }
