7a9b5f6aee6edb9c903c7bfada0b98dd4f5269b6
Lucy Suddenly <suddenlylucy@gmail.com>
2019-12-26 15:24:10 -0800

    loops!

--- hello.go
+++ hello.go
@@ -7,10 +7,14 @@ import (
 func main() {
 	// a := []int{5, 4, 3, 2, 1}
 	// a = append(a, 0)
-	object := make(map[string]int)
-	object["key1"] = 1
-	object["key2"] = 2
-	object["banana"] = 9999
+	// object := make(map[string]int)
+	// object["key1"] = 1
+	// object["key2"] = 2
+	// object["banana"] = 9999
 
-	fmt.Println(object)
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+
+	// fmt.Println(object)
 }
