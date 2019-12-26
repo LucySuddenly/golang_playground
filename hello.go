692f79f515ae42775d98f4638d6fee8cd22e42e1
Lucy Suddenly <suddenlylucy@gmail.com>
2019-12-26 15:30:38 -0800

    more functions and errors!

--- hello.go
+++ hello.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"math"
 )
 
 func main() {
@@ -11,10 +13,22 @@ func main() {
 	// object["key1"] = 1
 	// object["key2"] = 2
 	// object["banana"] = 9999
-
-	for i := 0; i < 5; i++ {
-		fmt.Println(i)
-	}
-
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println(i)
+	// }
 	// fmt.Println(object)
+	result, err := sqrt(6)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(result)
+	}
+}
+
+func sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, errors.New("No negative numbers")
+	}
+	return math.Sqrt(x), nil
 }
